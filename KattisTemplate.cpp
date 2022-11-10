fc98e1939ead99439d6f77bc2fb02d592d95e397
Dj <darrinmiller95@gmail.com>
2022-11-10 12:08:38 -0700

    added a new solution

--- KattisTemplate.cpp
+++ KattisTemplate.cpp
@@ -1,22 +1,11 @@
-#include <iostream>
-#include <cmath>
-#include <string>
-#include <vector>
-#include <stack>
-#include <deque>
-#include <queue>
-#include <sstream>
-#include <map>
-#include <set>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
     //Variables
-    int x,y,z;
-    string trash;
-    char junk;
+    
 
     //Code
 
